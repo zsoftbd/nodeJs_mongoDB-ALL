0fa08e0d84d2afbfa334dd08bad6f6edf687d2b4
zsoftbd <mzeaulkarim811@gmail.com>
2023-07-02 12:23:56 +0600

    content update

--- sendDataFromHtmlFile/sandData from-nodeTOmongoDB/index.js
+++ sendDataFromHtmlFile/sandData from-nodeTOmongoDB/index.js
@@ -16,12 +16,6 @@ async function run() {
     const db = client.db("admin2");
     const coll = db.collection("user2");
 
-    // insert code goes here
-    const pruducts = [
-      {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
-      {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
-      {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
-    ];
 
     const result = await coll.insertMany(pruducts);
     console.log('databse cunnacted')
